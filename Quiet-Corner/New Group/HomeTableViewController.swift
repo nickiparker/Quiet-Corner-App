bdcc321a3e1fe218237514d49e9cb7540b669bd3
Nicki Parker <NP204409@falmouth.ac.uk>
2019-04-09 18:28:03 +0100

    Add home list screen to display locations stored in the database

--- Quiet-Corner/New Group/HomeTableViewController.swift
+++ Quiet-Corner/New Group/HomeTableViewController.swift
@@ -7,86 +7,87 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeTableViewController: UITableViewController {
-        
+
+    // locations firebase
+    let db = Firestore.firestore()
+    private var documents: [DocumentSnapshot] = []
+    public var locations: [Location] = []
+    private var listener : ListenerRegistration!
+    
+   // Future implementation for user specific functionality
+   // var user: User!
+   // var userCountBarButtonItem: UIBarButtonItem!
+
     @IBAction func toFilterListButton(_ sender: UIBarButtonItem) {
     }
+ 
+    // Firebase global setter to constrain firebase query to max 50
+    fileprivate func baseQuery() -> Query {
+        return Firestore.firestore().collection("locations").limit(to: 50)
+    }
+    
+    fileprivate var query: Query? {
+        didSet {
+            if let listener = listener {
+                listener.remove()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.query = baseQuery() // firebase db query
     }
-
-    // MARK: - Table view data source
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.listener.remove()
+    }
+    
+     override func viewWillAppear(_ animated: Bool) {
+        self.listener =  query?.addSnapshotListener { (documents, error) in
+            guard let snapshot = documents else {
+                print("Error fetching documents results: \(error!)")
+                return
+            }
+            
+            let results = snapshot.documents.map { (document) -> Location in
+                if let location = Location(dictionary: document.data(), id: document.documentID) {
+                    return location
+                } else {
+                    fatalError("Unable to initialize type \(Location.self) with dictionary \(document.data())")
+                }
+            }
+            
+            print("Locations: \n", results)
+            print("Documents: \n", snapshot.documents)
+            self.locations = results
+            self.documents = snapshot.documents
+            self.tableView.reloadData()
+            
+        }
+    }
+    
+    // Build out the table of locations
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return locations.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath) as! LocationTableViewCell
+    
+        let location = locations[indexPath.row]
+        
+        cell.locationLabel!.text = location.location
+        cell.descriptionLabel!.text = location.description
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
---
+++ Quiet-Corner/New Group/LocationTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Quiet-Corner
+//
+//  Created by NICKI PARKER on 09/04/2019.
+//  Copyright © 2019 Nickiparker. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
+
