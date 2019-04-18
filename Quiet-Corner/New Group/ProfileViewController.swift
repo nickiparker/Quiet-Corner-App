33d333a98b07b3dbedeaf6db7ee3d826e1f7b7dd
Nicki Parker <NP204409@falmouth.ac.uk>
2019-04-18 15:43:13 +0100

    Add log out to profile

---
+++ Quiet-Corner/New Group/ProfileViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ProfileViewController.swift
+//  Quiet-Corner
+//
+//  Created by JODIE PARKER on 18/04/2019.
+//  Copyright © 2019 Nickiparker. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getUserProfile()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+       // ...
+    }
+    
+    func getUserProfile(){
+        if Auth.auth().currentUser != nil {
+            guard let userEmail = Auth.auth().currentUser?.email else { print("returning"); return }
+            self.emailLabel.text = userEmail
+        }
+    }
+
+    @IBAction func logOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("Error: ", error)
+        }
+        
+        UserDefaults.standard.set(false, forKey: "isLoggedIn")
+        UserDefaults.standard.synchronize()
+        
+        performSegue(withIdentifier: "unwindToSplashView", sender: self)
+    }
+    
+}
