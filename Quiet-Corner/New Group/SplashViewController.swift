4d408f8399df8f8af91f32b449d5831dd26bff7f
Nicki Parker <NP204409@falmouth.ac.uk>
2019-04-29 16:52:58 +0100

    Add logo to splash screen

--- Quiet-Corner/New Group/SplashViewController.swift
+++ Quiet-Corner/New Group/SplashViewController.swift
@@ -83,7 +83,7 @@ extension UIButton {
         self.layer.cornerRadius = 5
         self.setTitleColor(UIColor.white, for: .normal)
         self.layer.shadowColor = UIColor.darkGray.cgColor
-        self.layer.shadowRadius = 4
+        self.layer.shadowRadius = 3
         self.layer.shadowOpacity = 0.5
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
