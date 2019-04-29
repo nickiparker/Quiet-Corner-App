c8bfcb55bf209533539b89101b524564932b5408
Nicki Parker <NP204409@falmouth.ac.uk>
2019-04-29 21:58:34 +0100

    Make tabbar use white active/inactive icons

--- Quiet-Corner/Models/TabBarController.swift
+++ Quiet-Corner/Models/TabBarController.swift
@@ -2,7 +2,7 @@
 //  TabBarControllerViewController.swift
 //  Quiet-Corner
 //
-//  Created by JODIE PARKER on 28/04/2019.
+//  Created by NICKI PARKER on 28/04/2019.
 //  Copyright © 2019 Nickiparker. All rights reserved.
 //
 
@@ -14,7 +14,10 @@ class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Make unselected tabbar image white
+        tabBar.unselectedItemTintColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 1.0)
+        
         // Do any additional setup after loading the view.
     }
     
